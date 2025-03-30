90317aa3f9705e53123eb3b043dcab18051844c2
WindowsXpguy <theaverageuser404@gmail.com>
2025-03-29 21:08:57 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,88 @@
+export const games = [
+    {
+        title: "Classic Minecraft",
+        description: "Play the original browser version of Minecraft Classic!",
+        link: "https://classic.minecraft.net/"
+    },
+    {
+        title: "EaglercraftX",
+        description: "Play Minecraft 1.8.8 in your browser!",
+        link: "https://eaglercraft.net/"
+    },
+    {
+        title: "Minecraft Tower Defense",
+        description: "Defend your base against waves of monsters!",
+        link: "https://scratch.mit.edu/projects/10128407/"
+    },
+    {
+        title: "More Coming Soon",
+        description: "Stay tuned for more gaming content!",
+        link: "#"
+    }
+];
+
+export const servers = [
+    {
+        name: "EaglercraftX Official",
+        address: "wss://eaglercraft.net/server",
+        javaAddress: "eaglercraft.net",
+        version: "1.8.8",
+        description: "Official EaglercraftX server"
+    },
+    {
+        name: "Asspixel",
+        address: "wss://web.asspixel.net/CAP/",
+        javaAddress: "web.asspixel.net",
+        version: "1.8.8",
+        description: "Popular EaglercraftX server with mini-games"
+    },
+    {
+        name: "Zentic",
+        address: "wss://zentic.cc",
+        javaAddress: "zentic.cc",
+        version: "1.8.8",
+        description: "Zentic Network - Survival and more!"
+    },
+    {
+        name: "Ayunboom",
+        address: "wss://sus.shhnowisnottheti.me",
+        javaAddress: "sus.shhnowisnottheti.me",
+        version: "1.8.8",
+        description: "Ayunboom Network - Survival, Minigames, and more!"
+    },
+    {
+        name: "Archaic",
+        address: "wss://mc.arch.lol",
+        javaAddress: "mc.arch.lol",
+        version: "1.8.8",
+        description: "Archaic Network - Multiple gamemodes!"
+    },
+    {
+        name: "Asianf4rmer",
+        address: "wss://asianf4rmer.minecraft.pe",
+        javaAddress: "asianf4rmer.minecraft.pe",
+        version: "1.8.8",
+        description: "Asianf4rmer Network - PvP and more!"
+    },
+    {
+        name: "JexNetwork",
+        address: "wss://jex.minecraft.pe",
+        javaAddress: "jex.minecraft.pe",
+        version: "1.8.8",
+        description: "Jex Network - PvP focused server"
+    },
+    {
+        name: "Crosby",
+        address: "wss://crosby.minecraft.pe",
+        javaAddress: "crosby.minecraft.pe",
+        version: "1.8.8",
+        description: "Crosby Network - Fun minigames!"
+    },
+    {
+        name: "LunarCraft",
+        address: "wss://lunarcraft.minecraft.pe",
+        javaAddress: "lunarcraft.minecraft.pe",
+        version: "1.8.8",
+        description: "LunarCraft - Multiple server options"
+    }
+];
